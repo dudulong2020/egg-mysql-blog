91c572a7359fd7e83403328ba9039f957bc0800b
dudulong2020 <2414784235@qq.com>
2021-12-07 21:07:44 +0800

    编写获取文章详情/导航菜单数据接口

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -14,15 +14,9 @@ class HomeController extends Controller {
 
   async getArticleList() {
 
-    // let sql = 'SELECT article.id as id,' +
-    //   'article.title as title,' +
-    //   'article.introduce as introduce,' +
-    //   'article.addTime as addTime,' +
-    //   'article.view_count as view_count ,' +
-    //   '.type.typeName as typeName ' +
-    //   'FROM article LEFT JOIN type ON article.type_id = type.Id'
+    // let sql = 'SELECT article.id as id,' + 'article.title as title,' + 'article.introduce as introduce,' + "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," + 'article.view_count as view_count ,' + '.type.typeName as typeName ' + 'FROM article LEFT JOIN type ON article.type_id = type.Id'
 
-    // let sql = 'SELECT id,title,introduce,addtime,article_content,typeName,view_count FROM article'
+    // const results = await this.app.mysql.query(sql)
 
     const results = await this.app.mysql.query('select * from article')
 
@@ -31,6 +25,21 @@ class HomeController extends Controller {
     }
   }
 
+  async getArticleListById() {
+    //先配置路由的动态传值，然后再接收值
+    let id = this.ctx.params.id
+
+    let sql = 'SELECT article.id as id,' + 'article.title as title,' + 'article.introduce as introduce,' + 'article.content as content,' + "article.addTime as addTime," + 'article.view_count as view_count ,' + 'type.typeName as typeName ,' + 'type.id as typeId ' + 'FROM article LEFT JOIN type ON article.type_id = type.Id ' + 'WHERE article.id=' + id
+
+    const result = await this.app.mysql.query(sql)
+
+    this.ctx.body = { data: result }
+  }
+
+  async getTypeInfo() {
+    const result = await this.app.mysql.select('type')
+    this.ctx.body = { data: result }
+  }
 
 }
 
--- app/router/default.js
+++ app/router/default.js
@@ -2,4 +2,6 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/default/index', controller.default.home.index);
   router.get('/default/getArticleList',controller.default.home.getArticleList)
+  router.get('/default/getArticleListById/:id',controller.default.home.getArticleListById)
+  router.get('/default/getTypeInfo',controller.default.home.getTypeInfo)
 }
